1b7a08c655652ce89e9dcf241c9dd4fa2b58750c
Bluette1 <marylene.sawyer@gmail.com>
2020-05-12 15:58:38 +0200

    Adding `require 'minitest/pride` to test file and removing it from the source code
    - Adding `require 'minitest/pride` to test file and removing it from the
    source c-ode

--- hello-world/hello_world.rb
+++ hello-world/hello_world.rb
@@ -6,8 +6,6 @@ To get started with TDD, see the `README.md` file in your
 `ruby/hello-world` directory.
 =end
 
-require 'minitest/pride'
-
 class HelloWorld
     def self.hello
     'Hello, World!'
