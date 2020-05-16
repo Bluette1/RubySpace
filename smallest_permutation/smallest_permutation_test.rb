1e6dc071f80603ef93c0e73ea10fcfd5ba4c8df8
Bluette1 <marylene.sawyer@gmail.com>
2020-05-16 19:14:33 +0200

    Adding linter and stickler-ci
    - Adding linter and stickler-ci

--- smallest_permutation/smallest_permutation_test.rb
+++ smallest_permutation/smallest_permutation_test.rb
@@ -1,23 +1,21 @@
 begin
-    gem 'minitest', '>= 5.0.0'
-    require 'minitest/autorun'
-    require 'minitest/pride'
-    require_relative 'smallest_permutation'
+  gem 'minitest', '>= 5.0.0'
+  require 'minitest/autorun'
+  require 'minitest/pride'
+  require_relative 'smallest_permutation'
 rescue Gem::LoadError => e
-    puts "\nMissing Dependency:\n#{e.backtrace.first} #{e.message}"
-    puts 'Minitest 5.0 gem must be installed for the Ruby track.'
+  puts "\nMissing Dependency:\n#{e.backtrace.first} #{e.message}"
+  puts 'Minitest 5.0 gem must be installed for the Ruby track.'
 rescue LoadError => e
-    puts "\nError:\n#{e.backtrace.first} #{e.message}"
-    puts DATA.read
-    exit 1
+  puts "\nError:\n#{e.backtrace.first} #{e.message}"
+  puts DATA.read
+  exit 1
 end
-  
+
 class SmallestPermutationTest < Minitest::Test
-
-    def test_find_smallest
-      assert_equal 304689, SmallestPermutation.find_smallest(846903) 
-      assert_equal 1004568 , SmallestPermutation.find_smallest(5468001) 
-      assert_equal 1345, SmallestPermutation.find_smallest(5341)
-    end
-
+  def test_find_smallest
+    assert_equal 304_689, SmallestPermutation.find_smallest(846_903)
+    assert_equal 1_004_568, SmallestPermutation.find_smallest(5_468_001)
+    assert_equal 1345, SmallestPermutation.find_smallest(5341)
+  end
 end
