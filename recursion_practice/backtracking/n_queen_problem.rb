12e7123465bcb799a4f52c48b6a276e62162e5e1
Bluette1 <marylene.sawyer@gmail.com>
2021-02-28 21:00:13 +0200

    Clean up, remove unnecessary comments

--- recursion_practice/backtracking/n_queen_problem.rb
+++ recursion_practice/backtracking/n_queen_problem.rb
@@ -4,7 +4,7 @@
 # https://www.geeksforgeeks.org/n-queen-problem-backtracking-3/
 class NQueenProblem
   N = 4
-  # A utility function to print solution
+
   def print(board)
     board_str = ''
     (0..board.length - 1).each do |row|
@@ -17,19 +17,13 @@ class NQueenProblem
     board_str
   end
 
-  # A utility function to check if a queen can
-  #      be placed on board[row][col]. Note that this
-  #      function is called when "col" queens are already
-  #      placed in columns from 0 to col - 1. So we need
-  #      to check only left side for attacking queens
   # rubocop:disable Metrics/CyclomaticComplexity
   # rubocop:disable Metrics/PerceivedComplexity
   def safe(board, row, col)
-    # Check this row on left side
     (0..col - 1).each do |idx|
       return false if board[row][idx] == 1
     end
-    # Check upper diagonal on left side
+
     rw = row
     cl = col
     while rw >= 0 && cl >= 0
@@ -38,7 +32,7 @@ class NQueenProblem
       rw -= 1
       cl -= 1
     end
-    # Check lower diagonal on left side
+
     rw = row
     cl = col
     while rw < N && cl >= 0
@@ -52,43 +46,19 @@ class NQueenProblem
   # rubocop:enable Metrics/CyclomaticComplexity
   # rubocop:enable Metrics/PerceivedComplexity
 
-  # A recursive utility function to solve N
-  # Queen problem
   def solve_nq_util(board, col)
-    # base case: If all queens are placed
-    # then return true
     return true if col >= N
 
-    # Consider this column and try placing
-    # this queen in all rows one by one
     (0..N - 1).each do |idx|
-      # Check if the queen can be placed on
-      # board[i][col]
       next unless safe(board, idx, col)
 
-      # Place this queen in board[idx][col]
       board[idx][col] = 1
-      # recur to place rest of the queens
       return true if solve_nq_util(board, col + 1)
 
-      # If placing queen in board[i][col]
-      # doesn't lead to a solution then
-      # remove queen from board[idx][col] / BACKTRACK
       board[idx][col] = 0
     end
-    # If the queen can not be placed in any row in
-    # this colum col, then return false
     false
   end
-
-  # This function solves the N Queen problem using
-  #      Backtracking.  It mainly uses solve_nq_util () to
-  #      solve the problem. It returns false if queens
-  #      cannot be placed, otherwise, return true and
-  #      prints placement of queens in the form of 1s.
-  #      Please note that there may be more than one
-  #      solutions, this function prints one of the
-  #      feasible solutions.
 
   def solve_nq
     board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
