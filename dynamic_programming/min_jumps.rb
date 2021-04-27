a94274adeec3186bbfdfc6035415cf70850c15be
Bluette1 <marylene.sawyer@gmail.com>
2021-04-27 20:41:11 +0200

    Testing, fix lint errors

--- dynamic_programming/min_jumps.rb
+++ dynamic_programming/min_jumps.rb
@@ -1,26 +1,29 @@
-# The Problem: Given an array of non-negative integers, startIdx 
-# from the first element and reach the last by jumping. The jump 
-# length can be at most the value at the current position in the array. 
-# The optimum result is when you reach the goal using a minimum number 
+# The Problem: Given an array of non-negative integers, start_idx
+# from the first element and reach the last by jumping. The jump
+# length can be at most the value at the current position in the array.
+# The optimum result is when you reach the goal using a minimum number
 # of jumps.
-# First natural/bruteforce solution: minimum_jumps(startIdx, endIdx) = MIN(minimum_jumps(i, endIdx) for all i that are reachable from ‘startIdx’).
+# First natural/bruteforce solution: minimum_jumps(start_idx, end_idx) = MIN(minimum_jumps(i, end_idx)
+# for all i that are reachable from start_idx).
 
-def minimum_jumps(startIdx, endIdx, arr)
-  return 0 if arr[startIdx] == arr[endIdx]
+def minimum_jumps(start_idx, end_idx, arr)
+  return 0 if start_idx == end_idx
+
   min_jumps = Float::INFINITY
-  return min_jumps if arr[startIdx] == 0
+  return min_jumps if arr[start_idx].zero?
 
   # Calculate the minimum jumps for all positions i,
-  # that are reachable from the startIdx, and in turn calculate
+  # that are reachable from the start_idx, and in turn calculate
   # the minimum of the minimum_jumps for each of those indices.
 
-  (startIdx + 1..endIdx).each do |idx|
-    if (idx  <= startIdx + arr[startIdx])
-      jumps = minimum_jumps(idx, endIdx, arr)
-      min_jumps = jumps + 1 if (jumps + 1) < min_jumps
+  (start_idx + 1..end_idx).each do |idx|
+    if idx <= start_idx + arr[start_idx]
+      jumps = minimum_jumps(idx, end_idx, arr)
+      min_jumps = (jumps + 1) if (jumps + 1) < min_jumps
     end
   end
-  return min_jumps
+  min_jumps
 end
 
-p minimum_jumps(0, 4, [2, 3, 1, 1, 4])
+# p minimum_jumps(0, 4, [2, 3, 1, 1, 4]);
+p minimum_jumps(0, 9, [2, 3, 1, 1, 2, 4, 2, 0, 1, 1])
