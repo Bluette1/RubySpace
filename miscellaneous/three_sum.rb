2a2e25b242a16e4272b66880810a92827e67f496
Bluette1 <marylene.sawyer@gmail.com>
2021-06-02 13:26:39 +0200

    Add the working version
    - Fix lint errors

--- miscellaneous/three_sum.rb
+++ miscellaneous/three_sum.rb
@@ -1,3 +1,15 @@
+def check_valid(result, nums)
+  valid = []
+  (0..result.length - 1).each do |idx|
+    tupple = result[idx]
+    is_valid = true
+    (0..tupple.length - 1).each do |posn|
+      is_valid = false if nums.count(tupple[posn]) < tupple.count(tupple[posn]) || valid.include?(tupple)
+    end
+    valid << tupple if is_valid
+  end
+  valid
+end
 
 def dynamic_two_sum(nums, target)
   sum_hash = {}
@@ -6,7 +18,6 @@ def dynamic_two_sum(nums, target)
     if sum_hash[target - nums[idx]]
       # We've found the corresponding addend
       ans << [sum_hash[target - nums[idx]], idx]
-      # break
     else
       # Store this value in the hash
       sum_hash[nums[idx]] = idx
@@ -21,18 +32,18 @@ end
 
 def three_sum(nums)
   result = []
-  
+
   (0..nums.length - 1).each do |idx|
     target = -1 * nums[idx]
-    p 'target', target
     tuples = dynamic_two_sum(nums, target)
-    p 'tuples ', tuples
     tuples.each do |tuple|
       tuple << nums[idx]
+      tuple.sort!
       result << tuple
     end
   end
-  result
+  # p 'RESULT', result
+  check_valid(result, nums)
 end
 
 # Example 1:
@@ -40,7 +51,8 @@ end
 # Input: nums = [-1,0,1,2,-1,-4]
 # Output: [[-1,-1,2],[-1,0,1]]
 
-nums = [-1,0,1,2,-1,-4]
+# nums = [-1, 0, 1, 2, -1, -4]
+nums = [3,0,-2,-1,1,2]
 # nums = []
 # nums = [0]
 
